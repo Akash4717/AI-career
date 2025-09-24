b01fb3a42bcb680f3e4f6d280b4a4a4268239783
Akash4717 <akashagadi1910@gmail.com>
2025-09-24 18:54:29 +0530

    Initial commit

---
+++ prisma/migrations/20250924130608_change_demandlevel_to_string/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "IndustryInsight" ALTER COLUMN "demandLevel" SET NOT NULL,
+ALTER COLUMN "demandLevel" SET DATA TYPE TEXT,
+ALTER COLUMN "marketOutlook" SET NOT NULL,
+ALTER COLUMN "marketOutlook" SET DATA TYPE TEXT;
